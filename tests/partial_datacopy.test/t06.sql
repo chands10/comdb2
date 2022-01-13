b6218845e8454b1a493b793f2f558a2e558094b7
Salil Chandra <schandra107@bloomberg.net>
2022-01-13 18:22:47 +0000

    Added inserts and selects to partial datacopy tests
    Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

--- tests/partial_datacopy.test/t06.sql
+++ tests/partial_datacopy.test/t06.sql
@@ -14,4 +14,8 @@ create table t {
 select * from comdb2_keys where tablename='t';
 select * from comdb2_partial_datacopies where tablename='t';
 
+insert into t with r(n) as (select 1 union all select n + 4 from r where n < 97) select n, n + 1, n + 2, n + 3 from r
+select * from t where a < 40
+select a, b, c from t where a < 40
+
 drop table t;

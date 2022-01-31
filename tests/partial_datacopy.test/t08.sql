bf8c49d03821d079ca67f0bb39edfa6652c05795
Salil Chandra <schandra107@bloomberg.net>
2022-01-31 21:49:40 +0000

    Updated test to use small vutf8 size
    Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

--- tests/partial_datacopy.test/t08.sql
+++ tests/partial_datacopy.test/t08.sql
@@ -18,4 +18,23 @@ insert into t with recursive r(n) as (select 1 union all select n + 4 from r whe
 select * from t where a < 40;
 select a, b, c from t where a < 40;
 
+alter table t {
+    schema {
+        int a
+        vutf8 b[4]
+        int c
+        int d
+    }
+    keys {
+        datacopy(b, c) "a" = a
+    }
+};$$
+
+select * from comdb2_keys where tablename='t';
+select * from comdb2_partial_datacopies where tablename='t';
+
+select * from t where a < 40;
+select a, b, c from t where a < 40;
+
+
 drop table t;

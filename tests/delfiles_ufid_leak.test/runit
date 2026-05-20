chands10/comdb2

commit 5802570a946396719dbf1d0afcc49833251297e4

{180887345}: clearing ufid after schema change
This patch ensures that, after a schema change, any DB handles held by the ufid-hash
for the old files, are closed as well, such that these files won't have an open
reference in the bufferpool and therefore can be deleted correctly.

This patch also adds the ability to keep track of open DB handles. The `send bdb dumpopendbs`
message trap will produce walkbacks of open DB handles in the system.

A new test is added in this patch, which validates that replicants do not have "deleted"
file descriptors after the master has run delfiles.

Signed-off-by: Rivers Zhang <hzhang320@bloomberg.net>

bdb/file.c
@@ -158,6 +158,7 @@ static int bdb_close_only_int(bdb_state_type *bdb_state, DB_TXN *tid,
 
 enum {
     BDB_CLOSE_FLAGS_FLUSH = 1,
+    BDB_CLOSE_FLAGS_CLR_UFID = 2,
 };
 static int bdb_close_only_flags(bdb_state_type *, DB_TXN *, int *bdberr, int flags);
 
@@ -1411,13 +1412,14 @@ static void net_stopthread_rtn(void *arg)
  * Hence this function will now never fail - although it may spit out errors.
  * After this is called, the db is closed.
  */
-static int close_dbs_int(bdb_state_type *bdb_state, DB_TXN *tid, int flags)
+static int close_dbs_int(bdb_state_type *bdb_state, DB_TXN *tid, int flags, int bdb_close_flags)
 {
     int rc;
     int i;
     int dtanum, strnum;
     u_int8_t fileid[21] = {0};
     char fid_str[41] = {0};
+    DB *dbp;
 
     if (gbl_import_mode && bulk_import_tmpdb_should_ignore_table(bdb_state->name)) {
         return 0;
@@ -1440,14 +1442,17 @@ static int close_dbs_int(bdb_state_type *bdb_state, DB_TXN *tid, int flags)
 
     for (dtanum = 0; dtanum < MAXDTAFILES; dtanum++) {
         for (strnum = 0; strnum < MAXDTASTRIPE; strnum++) {
-            if (bdb_state->dbp_data[dtanum][strnum]) {
-                bdb_state->dbp_data[dtanum][strnum]->get_fileid(
-                    bdb_state->dbp_data[dtanum][strnum], fileid);
+            if ((dbp = bdb_state->dbp_data[dtanum][strnum]) != NULL) {
+                dbp->get_fileid(dbp, fileid);
                 fileid_str(fileid, fid_str);
-                logmsg(LOGMSG_DEBUG, "%s:%d  closing fileid %s\n", __func__,
-                       __LINE__, fid_str);
-                rc = bdb_state->dbp_data[dtanum][strnum]->closetxn(
-                    bdb_state->dbp_data[dtanum][strnum], tid, flags);
+                logmsg(LOGMSG_DEBUG, "%s:%d  closing fileid %s\n", __func__, __LINE__, fid_str);
+                rc = dbp->clear_ufid_hash(dbp, tid, flags);
+                if (rc != 0) {
+                    logmsg(LOGMSG_WARN, "%s: error closing %s[%d][%d]: %d %s\n", __func__, bdb_state->name, dtanum,
+                           strnum, rc, db_strerror(rc));
+                    /* do not fail */
+                }
+                rc = dbp->closetxn(dbp, tid, flags);
                 if (0 != rc) {
                     logmsg(LOGMSG_ERROR,
                            "%s: error closing %s[%d][%d]: %d %s\n", __func__,
@@ -1467,13 +1472,19 @@ static int close_dbs_int(bdb_state_type *bdb_state, DB_TXN *tid, int flags)
         logmsg(LOGMSG_DEBUG, "%s:%d  looking through table %s numix %d\n",
                __func__, __LINE__, bdb_state->name, bdb_state->numix);
         for (i = 0; i < bdb_state->numix; i++) {
-            /*fprintf(stderr, "closing ix %d\n", i);*/
-            bdb_state->dbp_ix[i]->get_fileid(bdb_state->dbp_ix[i], fileid);
+            dbp = bdb_state->dbp_ix[i];
+            dbp->get_fileid(dbp, fileid);
             fileid_str(fileid, fid_str);
             logmsg(LOGMSG_DEBUG, "%s:%d closing fileid %s\n", __func__,
                    __LINE__, fid_str);
-            rc = bdb_state->dbp_ix[i]->closetxn(bdb_state->dbp_ix[i], tid,
-                                                flags);
+
+            rc = dbp->clear_ufid_hash(dbp, tid, flags);
+            if (rc != 0) {
+                logmsg(LOGMSG_WARN, "%s: error closing %s->dbp_ix[%d] %d %s\n", __func__, bdb_state->name, i, rc,
+                       db_strerror(rc));
+                /* don't fail */
+            }
+            rc = dbp->closetxn(bdb_state->dbp_ix[i], tid, flags);
             if (rc != 0) {
                 logmsg(LOGMSG_ERROR, "%s: error closing %s->dbp_ix[%d] %d %s\n",
                        __func__, bdb_state->name, i, rc, db_strerror(rc));
@@ -1494,17 +1505,17 @@ static int close_dbs_int(bdb_state_type *bdb_state, DB_TXN *tid, int flags)
 
 static int close_dbs(bdb_state_type *bdb_state)
 {
-    return close_dbs_int(bdb_state, NULL, DB_NOSYNC);
+    return close_dbs_int(bdb_state, NULL, DB_NOSYNC, 0);
 }
 
 static int close_dbs_txn(bdb_state_type *bdb_state, DB_TXN *txn)
 {
-    return close_dbs_int(bdb_state, txn, DB_NOSYNC);
+    return close_dbs_int(bdb_state, txn, DB_NOSYNC, 0);
 }
 
 static int close_dbs_flush(bdb_state_type *bdb_state)
 {
-    return close_dbs_int(bdb_state, NULL, 0);
+    return close_dbs_int(bdb_state, NULL, 0, 0);
 }
 
 int bdb_isopen(bdb_state_type *bdb_handle) { return bdb_handle->isopen; }
@@ -6539,7 +6550,6 @@ static int bdb_del_file(bdb_state_type *bdb_state, DB_TXN *tid, char *filename,
                         int *bdberr)
 {
     DB_ENV *dbenv;
-    DB *dbp;
     char transname[PATH_MAX];
     char *pname = bdb_trans(filename, transname);
     int rc = 0;
@@ -6550,14 +6560,7 @@ static int bdb_del_file(bdb_state_type *bdb_state, DB_TXN *tid, char *filename,
         dbenv = bdb_state->dbenv;
 
     if ((rc = access(pname, F_OK)) == 0) {
-        int rc;
-
-        if ((rc = db_create(&dbp, dbenv, 0)) == 0 &&
-            (rc = dbp->open(dbp, NULL, pname, NULL, DB_BTREE, DB_CLR_UFID, 0666)) == 0) {
-            dbp->close(dbp, DB_NOSYNC);
-        }
-
-        rc = dbenv->dbremove(dbenv, tid, filename, NULL, 0);
+        int rc = dbenv->dbremove(dbenv, tid, filename, NULL, 0);
         if (rc) {
            logmsg(LOGMSG_ERROR, "bdb_del_file: dbremove %s failed: %d %s\n", filename, rc,
                    db_strerror(rc));
@@ -6978,8 +6981,10 @@ static int bdb_close_only_flags(bdb_state_type *bdb_state, DB_TXN *tid, int *bdb
     /* close doesn't fail */
     if (flags & BDB_CLOSE_FLAGS_FLUSH) {
        close_dbs_flush(bdb_state);
+    } else if (flags & BDB_CLOSE_FLAGS_CLR_UFID) {
+        close_dbs_int(bdb_state, NULL, DB_NOSYNC, BDB_CLOSE_FLAGS_CLR_UFID);
     } else {
-       close_dbs(bdb_state);
+        close_dbs(bdb_state);
     }
 
     /* now remove myself from my parents list of children */
@@ -7013,7 +7018,7 @@ int bdb_close_only_sc(bdb_state_type *bdb_state, tran_type *tran, int *bdberr)
 
     BDB_READLOCK("bdb_close_only_sc");
 
-    rc = bdb_close_only_int(bdb_state, tran ? tran->tid : NULL, bdberr);
+    rc = bdb_close_only_flags(bdb_state, tran ? tran->tid : NULL, bdberr, BDB_CLOSE_FLAGS_CLR_UFID);
 
     BDB_RELLOCK();
 

tests/berkdb_file_leaks_rep.test/Makefile
@@ -1,0 +1,5 @@
+ifeq ($(TESTSROOTDIR),)
+  include ../testcase.mk
+else
+  include $(TESTSROOTDIR)/testcase.mk
+endif

tests/berkdb_file_leaks_rep.test/lrl.options
@@ -1,0 +1,5 @@
+setattr CHECKPOINTTIME 9999999
+setattr MEMPTRICKLEPERCENT 10
+ufid_add_on_collect 1
+rep_skip_recovery on
+dtastripe 1

tests/berkdb_file_leaks_rep.test/runit
@@ -22,8 +22,9 @@ source ${TESTSROOTDIR}/tools/cluster_utils.sh
 
 dbnm=$1
 
-replicant=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select comdb2_host()"`
 master=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select host from comdb2_cluster where is_master='Y'"`
+replicant=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select comdb2_host()"`
+the_other_replicant=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "SELECT host FROM comdb2_cluster WHERE host <> '$master' AND host <> '$replicant'"`
 
 cdb2sql ${CDB2_OPTIONS} $dbnm default "CREATE TABLE t1 (i integer, b blob)"
 sleep 2 # wait for the checkpoint thread to do its 1st checkpoint
@@ -45,65 +46,46 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Restarting the other node $the_other_replicant ..."
+kill_restart_node $the_other_replicant 10 1
+cdb2sql $dbnm --host $the_other_replicant "SELECT 1"
+if [ $? -ne 0 ]; then
+    echo 'db not up?' >&2
+    exit 1
+fi
+
 echo 'Before truncate ...'
 cdb2sql $dbnm --host $replicant "EXEC PROCEDURE sys.cmd.send('bdb cachestatall')" | grep -A1 blobs0
 cdb2sql $dbnm --host $master "TRUNCATE TABLE t1"
 echo 'After truncate ...'
 cdb2sql $dbnm --host $replicant "EXEC PROCEDURE sys.cmd.send('bdb cachestatall')" | grep -A1 blobs0
 
-echo "Waiting for $replicant to become new master"
-while true; do
-    is_replicant_new_master=`cdb2sql --tabs $dbnm --host $replicant "SELECT comdb2_host() = (select host from comdb2_cluster where is_master='Y')"`
-    if [ "$is_replicant_new_master" = "1" ]; then
-        master=$replicant
-        break
+for host in $CLUSTER; do
+    echo --- BEGIN WALKBACKS for $host ---
+    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}'`
+    echo DB pid is $dbpid
+    baseaddr=`ssh $host "pmap $dbpid" | grep comdb2$ | head -1 | awk '{printf "0x%s", $1}'`
+    cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob
+    stacks=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob`
+    nooffset=$(for n in `echo "$stacks"| awk '{for(i=2; i<=NF; ++i) {print $i}}'`; do printf "0x%x " $(($n-$baseaddr)); done)
+    addr2line -fsp -e $COMDB2_EXE $nooffset
+    echo --- END WALKBACKS for $host ---
+done
+
+cdb2sql -tabs $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('delfiles t1')"
+
+# None of the replicants should have deleted files after master delfiles
+for host in $CLUSTER; do
+    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}'`
+    echo DB pid is $dbpi
+    echo '--- BEGIN LSOF (deleted) for $host ---'
+    ssh $host "lsof -p $dbpid | grep deleted"
+    echo '--- END LSOF (deleted) for $host ---'
+    cnt=`ssh $host "lsof -p $dbpid | grep -c deleted"`
+    if [ "$cnt" != 0 ]; then
+	echo 'Replicant has deleted files! DB handles leaked!' >&2
+	exit 1
     fi
-
-    master=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select host from comdb2_cluster where is_master='Y'"`
-    cdb2sql $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('upgrade $replicant')"
-    sleep 5
-    master=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select host from comdb2_cluster where is_master='Y'"`
-    echo "Master swung to $master"
-
-    while true; do
-        cdb2sql $dbnm --host $replicant "SELECT 1" >/dev/null
-        if [ $? -eq 0 ]; then
-            break;
-        fi
-        sleep 1
-    done
-done
-
-echo BEFORE delfiles
-for host in $CLUSTER; do
-    echo --- BEGIN WALKBACKS for $host ---
-    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}'`
-    echo DB pid is $dbpid
-    baseaddr=`ssh $host "pmap $dbpid" | grep comdb2$ | head -1 | awk '{printf "0x%s", $1}'`
-    cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob
-    stacks=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob`
-    nooffset=$(for n in `echo "$stacks"| awk '{for(i=2; i<=NF; ++i) {print $i}}'`; do printf "0x%x " $(($n-$baseaddr)); done)
-    addr2line -fsp -e $COMDB2_EXE $nooffset
-    echo --- END WALKBACKS for $host ---
-done
-
-cdb2sql -tabs $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('delfiles t1')" | grep 'DB_FILEOPEN: Rename or remove while file is open'
-if [ $? -eq 0 ]; then
-    echo "DB handles leaked" >&2
-    exit 1
-fi
-
-echo AFTER delfiles
-for host in $CLUSTER; do
-    echo --- BEGIN WALKBACKS for $host ---
-    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}'`
-    echo DB pid is $dbpid
-    baseaddr=`ssh $host "pmap $dbpid" | grep comdb2$ | head -1 | awk '{printf "0x%s", $1}'`
-    cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob
-    stacks=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep XXX.t1 | grep blob`
-    nooffset=$(for n in `echo "$stacks"| awk '{for(i=2; i<=NF; ++i) {print $i}}'`; do printf "0x%x " $(($n-$baseaddr)); done)
-    addr2line -fsp -e $COMDB2_EXE $nooffset
-    echo --- END WALKBACKS for $host ---
 done
 
 echo "Success!"

tests/db_open_tracker.test/Makefile
@@ -1,0 +1,8 @@
+ifeq ($(TESTSROOTDIR),)
+  include ../testcase.mk
+else
+  include $(TESTSROOTDIR)/testcase.mk
+endif
+ifeq ($(TEST_TIMEOUT),)
+	export TEST_TIMEOUT=1m
+endif

tests/db_open_tracker.test/lrl.options
@@ -1,0 +1,2 @@
+track_db_open 1
+dtastripe 1

tests/db_open_tracker.test/runit
@@ -1,0 +1,50 @@
+#!/usr/bin/env bash
+bash -n "$0" | exit 1
+
+set -e
+dbnm=$1
+
+host=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm default "SELECT host FROM comdb2_cluster WHERE is_master='Y'"`
+
+if [ -z "$CLUSTER" ] ; then
+    dbpid=`pgrep -a comdb2 | grep $dbnm | awk '{print $1}'`
+    echo DB pid is $dbpid
+    baseaddr=`pmap $dbpid | grep comdb2$ | head -1 | awk '{printf "0x%s", $1}'`
+else
+    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}'`
+    echo DB pid is $dbpid
+    baseaddr=`ssh $host "pmap $dbpid" | grep comdb2$ | head -1 | awk '{printf "0x%s", $1}'`
+fi
+echo base address is $baseaddr
+
+cdb2sql $dbnm --host $host "CREATE TABLE tbl_1 (i INTEGER)"
+
+stacks=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep tbl_1`
+if [ -z "$stacks" ] ; then
+    echo 'No output from bdb dumpopendbs???' >&2
+    exit 1
+fi
+
+nooffset=$(for n in `echo "$stacks"|awk '{for(i=2; i<=NF; ++i) {print $i}}'`; do printf "0x%x " $(($n-$baseaddr)); done)
+addr2line -fsp -e $COMDB2_EXE $nooffset
+# verify that we open this btree in add table code path
+addr2line -fsp -e $COMDB2_EXE $nooffset | grep -q add_table
+
+cdb2sql $dbnm --host $host "ALTER TABLE tbl_1 ADD COLUMN j INTEGER"
+
+stacks=`cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep tbl_1`
+if [ -z "$stacks" ] ; then
+    echo 'No output from bdb dumpopendbs???' >&2
+    exit 1
+fi
+
+nooffset=$(for n in `echo "$stacks"|awk '{for(i=2; i<=NF; ++i) {print $i}}'`; do printf "0x%x " $(($n-$baseaddr)); done)
+addr2line -fsp -e $COMDB2_EXE $nooffset
+# verify that we close previous btree from add_table code path,
+# and we open this btree in alter_table code path
+! addr2line -fsp -e $COMDB2_EXE $nooffset | grep -q add_table
+addr2line -fsp -e $COMDB2_EXE $nooffset | grep -q alter_table
+
+cdb2sql $dbnm --host $host "DROP TABLE tbl_1"
+# verify that we do not keep the btree around after it's dropped
+! cdb2sql ${CDB2_OPTIONS} -s --tabs $dbnm --host $host 'EXEC PROCEDURE sys.cmd.send("bdb dumpopendbs")' | grep -q tbl_1

bdb/info.c
@@ -1267,6 +1267,8 @@ static char *tokdup_hostname(char *tok, int ltok)
     return host;
 }
 
+extern void __db_dump_open(FILE *);
+
 void bdb_process_user_command(bdb_state_type *bdb_state, char *line, int lline,
                               int st)
 {
@@ -1911,6 +1913,8 @@ void bdb_process_user_command(bdb_state_type *bdb_state, char *line, int lline,
         pgno = toknum(tok, ltok);
 
         __pgtrash(bdb_state->dbenv, fileid, pgno);
+    } else if (tokcmp(tok, ltok, "dumpopendbs") == 0) {
+        __db_dump_open(out);
     } else {
         logmsg(LOGMSG_ERROR, "backend engine unknown cmd <%.*s>. try help\n", ltok,
                 tok);

commit 77c50877644b9bd7723e4f4032c608101fd872e6

{184305174} Fix delfiles to clear ufid-hash before deleting files
This fixes a regression introduced in commit 5802570a9 where ufid clearing
was moved from bdb_del_file() to schema changes only (bdb_close_only_sc).
This caused the standalone 'delfiles' command to leave open file descriptors
in ufid-hash, especially on replicants that opened handles during recovery.

Root Cause:
- When replicants apply log records during recovery, they open DB handles
  via ufid-hash and mark them with DB_AM_RECOVER flag
- These handles are kept open for performance
- When 'delfiles' command runs WITHOUT a schema change, bdb_del_file()
  directly calls dbremove() without clearing ufid-hash entries
- The OS cannot release disk space because handles remain open
- Files show as "deleted" in lsof output but space is not reclaimed

Impact:
- Production databases accumulate "deleted" files over time
- Requires weekly database bounces to reclaim space

The Fix:
- Restore ufid clearing to bdb_del_file() before calling dbremove()
- Respects gbl_clear_ufid_on_db_close tunable (default=1)
- Opens the file briefly, calls clear_ufid_hash(), then closes it
- This clears any recovery handles (DB_AM_RECOVER) from ufid-hash
- Non-fatal if clearing fails - logs warning and continues

Test:
- New test delfiles_ufid_leak.test validates delfiles WITHOUT schema change
- Unlike berkdb_file_leaks_rep.test which does TRUNCATE before delfiles,
  this test reproduces the production scenario
- Verifies no "deleted" files remain open after delfiles

Signed-off-by: Salil Chandra <schandra107@bloomberg.net>

bdb/file.c
@@ -6554,6 +6554,7 @@ static int bdb_del_file(bdb_state_type *bdb_state, DB_TXN *tid, char *filename,
                         int *bdberr)
 {
     DB_ENV *dbenv;
+    DB *dbp = NULL;
     char transname[PATH_MAX];
     char *pname = bdb_trans(filename, transname);
     int rc = 0;
@@ -6564,7 +6565,31 @@ static int bdb_del_file(bdb_state_type *bdb_state, DB_TXN *tid, char *filename,
         dbenv = bdb_state->dbenv;
 
     if ((rc = access(pname, F_OK)) == 0) {
-        int rc = dbenv->dbremove(dbenv, tid, filename, NULL, 0);
+        extern int gbl_clear_ufid_on_db_close;
+
+        /* Clear any ufid-hash entries before deleting the file to prevent
+         * "deleted" file descriptors from being held open, especially on
+         * replicants that opened handles during recovery. */
+        if (gbl_clear_ufid_on_db_close) {
+            if ((rc = db_create(&dbp, dbenv, 0)) == 0 &&
+                (rc = dbp->open(dbp, NULL, pname, NULL, DB_BTREE, 0, 0666)) == 0) {
+                /* Clear ufid hash entry for this file */
+                int clear_rc = dbp->clear_ufid_hash(dbp, NULL, 0);
+                if (clear_rc != 0) {
+                    logmsg(LOGMSG_WARN, "bdb_del_file: clear_ufid_hash for %s failed: %d %s\n", filename, clear_rc,
+                           db_strerror(clear_rc));
+                    /* Continue anyway - not fatal */
+                }
+                dbp->close(dbp, DB_NOSYNC);
+                dbp = NULL;
+            } else if (rc != ENOENT) {
+                /* File exists but we couldn't open it - log but continue */
+                logmsg(LOGMSG_WARN, "bdb_del_file: failed to open %s for ufid clearing: %d %s\n", filename, rc,
+                       db_strerror(rc));
+            }
+        }
+
+        rc = dbenv->dbremove(dbenv, tid, filename, NULL, 0);
         if (rc) {
            logmsg(LOGMSG_ERROR, "bdb_del_file: dbremove %s failed: %d %s\n", filename, rc,
                    db_strerror(rc));

tests/delfiles_ufid_leak.test/Makefile
@@ -1,0 +1,5 @@
+ifeq ($(TESTSROOTDIR),)
+  include ../testcase.mk
+else
+  include $(TESTSROOTDIR)/testcase.mk
+endif

tests/delfiles_ufid_leak.test/lrl.options
@@ -1,0 +1,4 @@
+setattr CHECKPOINTTIME 9999999
+setattr MEMPTRICKLEPERCENT 10
+clear_ufid_on_db_close 1
+dtastripe 1

tests/delfiles_ufid_leak.test/runit
@@ -1,0 +1,91 @@
+#!/usr/bin/env bash
+bash -n "$0" | exit 1
+
+# This test validates that the delfiles command clears ufid-hash entries
+# BEFORE deleting files, preventing "deleted" file descriptors from being
+# held open on replicants.
+#
+# Unlike berkdb_file_leaks_rep.test which does TRUNCATE before delfiles,
+# this test runs delfiles WITHOUT any preceding schema change to reproduce
+# the issue where deleted files accumulate on replicants.
+
+source ${TESTSROOTDIR}/tools/cluster_utils.sh
+
+[ -z "${CLUSTER}" ] && { echo "Test requires a cluster"; exit 0; }
+
+dbnm=$1
+
+master=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select host from comdb2_cluster where is_master='Y'"`
+replicant=`cdb2sql --tabs ${CDB2_OPTIONS} $dbnm default "select comdb2_host()"`
+
+echo "Creating table and inserting data..."
+cdb2sql ${CDB2_OPTIONS} $dbnm default "CREATE TABLE t1 (i integer, b blob)"
+sleep 2
+
+cdb2sql $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('bdb checkpoint')"
+sleep 2
+
+# Insert data to create files
+cdb2sql ${CDB2_OPTIONS} $dbnm default "INSERT INTO t1 VALUES(1, x'DEADBEEF')"
+cdb2sql ${CDB2_OPTIONS} $dbnm default "INSERT INTO t1 VALUES(2, x'CAFEBABE')"
+sleep 2
+
+cdb2sql $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('bdb checkpoint')"
+sleep 2
+
+# TRUNCATE to create an old file version
+echo "Truncating table to create old file version..."
+cdb2sql ${CDB2_OPTIONS} $dbnm default "TRUNCATE TABLE t1"
+sleep 2
+
+cdb2sql $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('bdb checkpoint')"
+sleep 2
+
+# Insert new data in the new file version
+cdb2sql ${CDB2_OPTIONS} $dbnm default "INSERT INTO t1 VALUES(3, x'BAADF00D')"
+sleep 2
+
+cdb2sql $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('bdb checkpoint')"
+sleep 2
+
+echo "Restarting replicant $replicant to force recovery..."
+# This causes the replicant to open DB handles via ufid-hash during recovery
+# These handles will be for BOTH the old and new file versions
+kill_restart_node $replicant 10 1
+cdb2sql $dbnm --host $replicant "SELECT 1"
+if [ $? -ne 0 ]; then
+    echo 'db not up?' >&2
+    exit 1
+fi
+
+sleep 2
+
+echo "Running delfiles WITHOUT any schema change..."
+# KEY: No TRUNCATE/ALTER/DROP before delfiles!
+# This is the scenario where delfiles is run standalone
+# Without the fix, ufid-hash holds open handles to old files
+cdb2sql -tabs $dbnm --host $master "EXEC PROCEDURE sys.cmd.send('delfiles t1')"
+
+sleep 2
+
+# Check all nodes for "deleted" files
+echo "Checking for deleted files held open..."
+for host in $CLUSTER; do
+    dbpid=`ssh $host 'pgrep -a comdb2' | grep $dbnm | awk '{print $1}' | head -1`
+    echo "Checking host $host (pid $dbpid)..."
+
+    deleted_count=`ssh $host "lsof -p $dbpid 2>/dev/null | grep -c deleted"`
+    rc=$?
+    [ $rc -ne 0 ] && deleted_count=0
+
+    if [ "$deleted_count" != "0" ]; then
+        echo "ERROR: Host $host has $deleted_count deleted files held open!" >&2
+        echo "Deleted files:" >&2
+        ssh $host "lsof -p $dbpid 2>/dev/null | grep deleted" >&2
+        exit 1
+    fi
+    echo "  No deleted files on $host"
+done
+
+echo "Success! delfiles properly cleared ufid-hash entries."
+exit 0